5d3104da72f5a99639c379f4d44805a71d6d7962
analyceferreira <anallyce_@live.com>
2022-12-07 20:41:10 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import Scene from './Scene'
+
+export default function App() {
+  return (
+    <Suspense fallback={null}>
+      <Canvas shadows flat linear>
+        <Scene />
+        <OrbitControls
+        enableDamping="false"
+        />
+      </Canvas>
+    </Suspense>
+  )
+}
