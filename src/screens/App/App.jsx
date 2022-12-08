81d8c673047497099687cd1877b757fd4c380b32
analyceferreira <anallyce_@live.com>
2022-12-07 21:28:01 -0400

    REFACTOR | Folder structuring

--- src/screens/App/App.jsx
+++ src/screens/App/App.jsx
@@ -1,16 +1,14 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import Scene from './Scene'
+import Scene from '../../components/3DScene/Scene.jsx'
 
 export default function App() {
   return (
     <Suspense fallback={null}>
       <Canvas shadows flat linear>
         <Scene />
-        <OrbitControls
-        enableDamping="false"
-        />
+        <OrbitControls/>
       </Canvas>
     </Suspense>
   )
